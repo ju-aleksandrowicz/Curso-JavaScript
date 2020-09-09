575df2940e9eaccc4faa6f871d8966da7aef94d5
Julian Aleksandrowicz <62268641+ju-aleksandrowicz@users.noreply.github.com>
2020-09-09 19:38:35 -0300

    Ejercicio práctico
    - Sumar dos números.
    - Restar dos números.
    - Calcular precio con IVA.
    - Devolver el nombre completo de una persona.
    - Devolver el número más grande.
    - Devolver si el número es par o no.
    - Devolver si el número es mayor a 20 o no.
    - Devolver la primera palabra de una frase.
    - Capitalizar una frase.

--- funciones.js
+++ funciones.js
@@ -1,61 +1,60 @@
-
-//function sumarDosNumero(primerNumero, segundoNumero){
- //   var res = primerNumero + segundoNumero;
- //   return res;
-//}
-//var resultado = sumarDosNumero(1,2); //ejemplos de la sumar dos nums
-//console.log(resultado);
-//console.log(sumarDosNumero(5,10));
-//console.log(sumarDosNumero(6,19));
-
-
-//function conIVA(precio){  //funcion con IVA
-    //var precioConIVA = precio * 1.21;
-   // return precioConIVA;
-//}
-//console.log(conIVA(50));
+function sumarDosNumero(primerNumero, segundoNumero){
+    var res = primerNumero + segundoNumero;
+    return res;
+}
+var resultado = sumarDosNumero(1,2); //ejemplos de la sumar dos nums
+console.log(resultado);
+console.log(sumarDosNumero(5,10));
+console.log(sumarDosNumero(6,19));
 
 
-//function restarDosNumeros(primero,segundo){ //funcion para restar dos numeros
-//    var resultadoResta = primero - segundo;
-//    return resultadoResta;
-//}
-//console.log(restarDosNumeros(8,5));
+function conIVA(precio){  //funcion con IVA
+    var precioConIVA = precio * 1.21;
+    return precioConIVA;
+}
+console.log(conIVA(50));
 
 
-//function NombreCompleto(nombre,apellido){ //funcion de nombre completo
-//    var FullName = (nombre + " " + apellido);
-//    return FullName;
-//}
-//console.log(NombreCompleto("Juan","Gomez"));
+function restarDosNumeros(primero,segundo){ //funcion para restar dos numeros
+    var resultadoResta = primero - segundo;
+    return resultadoResta;
+}
+console.log(restarDosNumeros(8,5));
 
 
-//console.log(Math.max(10,9)); //num mas grande (func ya hecha js)
+function NombreCompleto(nombre,apellido){ //funcion de nombre completo
+    var FullName = (nombre + " " + apellido);
+    return FullName;
+}
+console.log(NombreCompleto("Juan","Gomez"));
 
 
-//function esPar(numero){ //saber si un numero es par
-//    if (numero%2 == 0)
-//        return true;
-//    else
-//        return false;
-//}
-//console.log("El primer numero ingresado es: ",esPar(26));
-//console.log("El segundo numero ingresado es: ",esPar(15));
+console.log(Math.max(10,9)); //num mas grande (func ya hecha js)
 
 
-//function mayorA20(numero){ //saber si es mayor que 20
-//    if (numero>20)
-//        return true;
-//    else
-//        return false;
-//}
-//console.log("Primera comparacion: ",mayorA20(46));
-//console.log("Segunda comparacion: ",mayorA20(5));
+function esPar(numero){ //saber si un numero es par
+    if (numero%2 == 0)
+        return true;
+    else
+        return false;
+}
+console.log("El primer numero ingresado es: ",esPar(26));
+console.log("El segundo numero ingresado es: ",esPar(15));
 
 
-//var frase = "esta es una prueba"; //capitalizar toda la frase
-//console.log(frase.toUpperCase());
+function mayorA20(numero){ //saber si es mayor que 20
+    if (numero>20)
+        return true;
+    else
+        return false;
+}
+console.log("Primera comparacion: ",mayorA20(46));
+console.log("Segunda comparacion: ",mayorA20(5));
 
 
-//var frase = "Esta es una frase de prueba"; //devuelve primera palabra
-//console.log(frase.split(" ",1));
+var frase = "esta es una prueba"; //capitalizar toda la frase
+console.log(frase.toUpperCase());
+
+
+var frase = "Esta es una frase de prueba"; //devuelve primera palabra
+console.log(frase.split(" ",1));
